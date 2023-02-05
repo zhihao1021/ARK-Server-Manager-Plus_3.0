031ddc12b3e47aef31426c08dd7f25c68ac93cd2
AloneAlongLife <xd0931021@gmail.com>
2023-02-05 23:19:52 +0800

    Auto Commit

--- protect.py
+++ protect.py
@@ -1,6 +1,6 @@
 from os import system
-from os.path import isfile, getctime
-from time import sleep
+from os.path import isfile, getmtime
+from time import sleep, time
 
 from psutil import AccessDenied, NoSuchProcess, Process
 
@@ -8,15 +8,15 @@ if __name__ == "__main__":
     last_change_time = 0
     while True:
         if isfile("PID"):
-            if getctime("PID") != last_change_time:
-                last_change_time = getctime("PID")
+            if getmtime("PID") != last_change_time:
+                last_change_time = getmtime("PID")
                 with open("PID", mode="r") as pid_file:
                     last_pid = int(pid_file.read().strip())
-                print(f"Running Process Change, PID: {last_pid}")
+                print(f"{format(time(), '.2f')} Running Process Change, PID: {last_pid}")
             try:
-                print(f"Waiting For PID: {last_pid}")
+                print(f"{format(time(), '.2f')} Waiting For PID: {last_pid}", end="\r")
                 Process(last_pid).wait()
-                print(f"PID - {last_pid} killed.")
+                print(f"\n{format(time(), '.2f')} PID - {last_pid} killed.")
                 system("start cmd /c \"start.cmd\"")
             except (NoSuchProcess, AccessDenied):
                 pass
